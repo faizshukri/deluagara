033c39d50a7a0f9d1002fe5071a0ed7c5d7ee509
Faiz Shukri <faizshukri90@gmail.com>
2015-07-02 14:20:28 +0100

    Refactor FaizGeoIP
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- app/Services/FaizGeoIP.php
+++ app/Services/FaizGeoIP.php
@@ -17,16 +17,18 @@ class FaizGeoIP implements GeoIP {
 
     public function __construct(Request $request){
         $this->request = $request;
+        $this->request->setTrustedProxies(array('127.0.0.1')); // only trust proxy headers coming from the IP addresses on the array (change this to suit your needs)
     }
 
     public function getLocation(){
-        $this->request->setTrustedProxies(array('127.0.0.1')); // only trust proxy headers coming from the IP addresses on the array (change this to suit your needs)
 
-        if(!in_array(substr($this->request->getClientIp(), 0, 7), ['::1', '192.168'])) {
+        if(!in_array(substr($this->request->getClientIp(), 0, 7), ['::1', '192.168']))
+        {
             $this->serviceUrl .= '/'. $this->request->getClientIp();
         }
 
         $json = json_decode(file_get_contents($this->serviceUrl), true);
+
         return [ 'latitude' => $json['latitude'], 'longitude' => $json['longitude'] ];
     }
 }
