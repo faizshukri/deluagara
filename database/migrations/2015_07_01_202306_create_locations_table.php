b0a0a6ad8c858dee330828924d3b9e287b3a8a5b
Faiz Shukri <faizshukri90@gmail.com>
2015-07-02 01:56:37 +0100

    Create location model
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

---
+++ database/migrations/2015_07_01_202306_create_locations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('address');
+            $table->string('postcode');
+            $table->integer('latitude');
+            $table->integer('longitude');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('locations');
+    }
+}
