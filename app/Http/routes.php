093567f764370b5a332aa21372dd2dd004b56505
Faiz Shukri <faizshukri90@gmail.com>
2015-07-02 02:48:24 +0100

    Add login and register view
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,6 +13,15 @@
 
 Route::get('/', 'HomeController@main');
 
+// Authentication routes...
+Route::get('auth/login', [ 'as' => 'auth.login', 'uses' => 'Auth\AuthController@getLogin' ]);
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', [ 'as' => 'auth.logout', 'uses' => 'Auth\AuthController@getLogout']);
+
+// Registration routes...
+Route::get('auth/register', [ 'as' => 'auth.register', 'uses' => 'Auth\AuthController@getRegister']);
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 Route::group(['prefix'=>'api/v1'], function(){
     Route::get('locations', 'Api\V1\LocationController@locations');
     Route::get('locations.geojson', 'Api\V1\LocationController@geolocations');
