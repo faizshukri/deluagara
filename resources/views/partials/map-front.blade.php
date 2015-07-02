6f0be19bf852f1e2b46bf3e9ef3ad22c69e74837
Faiz Shukri <faizshukri90@gmail.com>
2015-07-02 02:01:14 +0100

    Put marker into cluster, and remove border radius for all elements
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- resources/views/partials/map-front.blade.php
+++ resources/views/partials/map-front.blade.php
@@ -8,12 +8,24 @@
     <script>
         (function(){
 
+            var locations = {!! $locations->toJson() !!};
+
             initMap();
 
             function initMap() {
                 L.mapbox.accessToken = "{{ env('MAPBOX_ACCESSTOKEN')  }}";
                 var map = L.mapbox.map('map-front', 'mapbox.streets')
-                        .setView([ {{ $location['latitude'] }}, {{ $location['longitude'] }} ], 14);
+                        .setView([ {{ $user_coord['latitude'] }}, {{ $user_coord['longitude'] }} ], 13);
+
+                var featureLayer = L.mapbox.featureLayer()
+                    .loadURL('/api/v1/locations.geojson')
+                    .on('ready', function(e) {
+                        var clusterGroup = new L.MarkerClusterGroup();
+                        e.target.eachLayer(function(layer) {
+                            clusterGroup.addLayer(layer);
+                        });
+                        map.addLayer(clusterGroup);
+                    });
             };
         })();
 
