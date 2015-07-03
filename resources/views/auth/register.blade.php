61aaf9c860d5c14e1b55bef340e4cee52f75d016
Faiz Shukri <faizshukri90@gmail.com>
2015-07-03 04:20:27 +0100

    Create basic register page
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,31 +2,43 @@
 @extends('layouts.master')
 
 @section('content')
-    <form method="POST" action="/auth/register">
-        {!! csrf_field() !!}
 
-        <div class="col-md-6">
-            Name
-            <input type="text" name="name" value="{{ old('name') }}">
-        </div>
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-3">
+            <form role="form" method="POST" action="/auth/register">
+                {!! csrf_field() !!}
 
-        <div>
-            Email
-            <input type="email" name="email" value="{{ old('email') }}">
-        </div>
+                <h2>Please Sign Up <small>It's free and always will be.</small></h2>
+                <hr class="colorgraph">
+                <div class="form-group @if ($errors->has('name')) has-error @endif">
+                    <input type="text" name="name" value="{{ old('name') }}" id="name" class="form-control input-lg" placeholder="Full Name" tabindex="1">
+                    @if ($errors->has('name')) <p class="help-block">{{ $errors->first('name') }}</p> @endif
+                </div>
+                <div class="form-group @if ($errors->has('email')) has-error @endif">
+                    <input type="email" name="email" value="{{ old('email') }}" id="email" class="form-control input-lg" placeholder="Email Address" tabindex="2">
+                    @if ($errors->has('email')) <p class="help-block">{{ $errors->first('email') }}</p> @endif
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-sm-6 col-md-6">
+                        <div class="form-group @if ($errors->has('password')) has-error @endif">
+                            <input type="password" name="password" id="password" class="form-control input-lg" placeholder="Password" tabindex="3">
+                            @if ($errors->has('password')) <p class="help-block">{{ $errors->first('password') }}</p> @endif
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-6">
+                        <div class="form-group @if ($errors->has('password')) has-error @endif">
+                            <input type="password" name="password_confirmation" id="password_confirmation" class="form-control input-lg" placeholder="Confirm Password" tabindex="4">
+                        </div>
+                    </div>
+                </div>
 
-        <div>
-            Password
-            <input type="password" name="password">
+                <hr class="colorgraph">
+                <div class="row">
+                    <div class="col-xs-12 col-md-6"><input type="submit" value="Register" class="btn btn-primary btn-block btn-lg" tabindex="5"></div>
+                    <div class="col-xs-12 col-md-6"><a href="{{ route('auth.login') }}" class="btn btn-success btn-block btn-lg">Sign In</a></div>
+                </div>
+            </form>
         </div>
+    </div>
 
-        <div class="col-md-6">
-            Confirm Password
-            <input type="password" name="password_confirmation">
-        </div>
-
-        <div>
-            <button type="submit">Register</button>
-        </div>
-    </form>
 @endsection
