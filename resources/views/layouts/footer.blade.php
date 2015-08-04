b78b7c74a85d73e8ab9b46a343a64bbb5f686233
Faiz Shukri <faizshukri90@gmail.com>
2015-08-04 23:29:31 +0100

    Fix footer layout in mobile
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="container">
-    <div style="position: absolute; left: 0; right: 0;">Created by Faiz Shukri | &copy; 2015</div>
-    <span id="siteseal" class="pull-right" style="position: relative; z-index: 2; margin-top: -6px;"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=b3d7rY5ImdZU7jt7tYhb4goMCWnWB5FiXEriT6oCtPHFryVm1bhbFat8OhNs"></script></span>
+    <div id="developer">Created by Faiz Shukri | &copy; 2015</div>
+    <span id="siteseal" class="pull-right"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=b3d7rY5ImdZU7jt7tYhb4goMCWnWB5FiXEriT6oCtPHFryVm1bhbFat8OhNs"></script></span>
 </footer>
 
 <script src="{{ elixir('js/vendor.js') }}"></script>
