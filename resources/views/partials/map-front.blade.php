e6fe50cb9c0ce68a5aaf6c6d5b004a58ef1963a2
Faiz Shukri <faizshukri90@gmail.com>
2015-08-06 16:23:16 +0100

    Improve map pan, make it to center with the popup
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- resources/views/partials/map-front.blade.php
+++ resources/views/partials/map-front.blade.php
@@ -30,9 +30,13 @@
                             clusterGroup.addLayer(layer);
                         });
                         map.addLayer(clusterGroup);
-                    }).on('click', function(e) {
-                        map.panTo(e.layer.getLatLng());
                     });
+
+                map.on('popupopen', function(e){
+                    var px = map.project(e.popup._latlng); // find the pixel location on the map where the popup anchor is
+                    px.y -= e.popup._container.clientHeight/2; // find the height of the popup container, divide by 2, subtract from the Y axis of marker location
+                    map.panTo(map.unproject(px),{animate: true}); // pan to new center
+                });
             };
         })();
 
