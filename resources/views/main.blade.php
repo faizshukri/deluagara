422d2a959d441ce9e92b0d9e062b113712cd4c66
Faiz Shukri <faizshukri90@gmail.com>
2015-08-07 18:41:24 +0100

    Change front content
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,5 +7,33 @@
 @endsection
 
 @section('content')
-    <h1>Terbaiklah</h1>
+    <p class="clearfix">&nbsp;</p>
+    <div class="row">
+        <div class="col-sm-12" align="center">
+            <h1>Katsitu is a public listing for Malaysian residing abroad</h1>
+        </div>
+        <p class="clearfix">&nbsp;</p>
+        <div class="col-sm-12 features-wrapper">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div align="center">
+                        <img src="/images/features/map.png">
+                        <h2>Map</h2>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div align="center">
+                        <img src="/images/features/profile.png">
+                        <h2>Profile</h2>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div align="center">
+                        <img src="/images/features/people.png">
+                        <h2>People</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
