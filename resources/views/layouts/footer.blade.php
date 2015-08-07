9d326a155039405f2ecaace80ce2224024ddadcd
Faiz Shukri <faizshukri90@gmail.com>
2015-08-07 20:24:18 +0100

    Add more features

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,5 @@
+<p class="clearfix">&nbsp;</p>
+
 <footer class="container">
     <div id="developer">Created by Faiz Shukri | &copy; 2015</div>
     <span id="siteseal" class="pull-right"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=b3d7rY5ImdZU7jt7tYhb4goMCWnWB5FiXEriT6oCtPHFryVm1bhbFat8OhNs"></script></span>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -15,22 +15,40 @@
         <p class="clearfix">&nbsp;</p>
         <div class="col-sm-12 features-wrapper">
             <div class="row">
-                <div class="col-sm-4">
+                <div class="col-sm-4 col-xs-6">
                     <div align="center">
                         <img src="/images/features/map.png">
                         <h2>Map</h2>
                     </div>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-4 col-xs-6">
                     <div align="center">
                         <img src="/images/features/profile.png">
                         <h2>Profile</h2>
                     </div>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-4 col-xs-6">
                     <div align="center">
                         <img src="/images/features/people.png">
                         <h2>People</h2>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-6">
+                    <div align="center">
+                        <img src="/images/features/messaging.png">
+                        <h2>Messaging</h2>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-6">
+                    <div align="center">
+                        <img src="/images/features/event.png">
+                        <h2>Event</h2>
+                    </div>
+                </div>
+                <div class="col-sm-4 col-xs-6">
+                    <div align="center">
+                        <img src="/images/features/accomodation.png">
+                        <h2>Accomodation</h2>
                     </div>
                 </div>
             </div>
