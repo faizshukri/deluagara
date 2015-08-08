066896c4aeb77b9a0c5b3ca3991dbcec92cb7204
Faiz Shukri <faizshukri90@gmail.com>
2015-08-08 04:09:28 +0100

    Add front page information and improve footer esp for mobile
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,14 @@
 <p class="clearfix">&nbsp;</p>
 
+<hr style="margin: 0px;"/>
 <footer class="container">
-    <div id="developer">Created by Faiz Shukri | &copy; 2015</div>
+    <div class="pull-left" id="nav-bottom">
+        <ul class="nav navbar-nav">
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+    <div id="developer">Katsitu.Com &copy; 2015 | All rights reserved</div>
     <span id="siteseal" class="pull-right"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=b3d7rY5ImdZU7jt7tYhb4goMCWnWB5FiXEriT6oCtPHFryVm1bhbFat8OhNs"></script></span>
 </footer>
 
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -10,7 +10,8 @@
     <p class="clearfix">&nbsp;</p>
     <div class="row">
         <div class="col-sm-12" align="center">
-            <h1>Katsitu is a public listing website for Malaysian community that were residing abroad</h1>
+            <h1>Katsitu is a public listing website for Malaysian community residing
+                <span style="text-decoration: line-through;">abroad.</span> in <span style="font-weight: normal;">United Kingdom</span>.</h1>
         </div>
         <p class="clearfix">&nbsp;</p>
         <div class="col-sm-12 features-wrapper">
@@ -19,36 +20,48 @@
                     <div align="center">
                         <img src="/images/features/map.png">
                         <h2>Map</h2>
+                        <p>Get to know Malaysian population around UK in bird's eye view.</p>
                     </div>
                 </div>
                 <div class="col-sm-4 col-xs-6">
                     <div align="center">
                         <img src="/images/features/profile.png">
                         <h2>Profile</h2>
+                        <p>A public, clean and attractive profile page for every Katsitu members.</p>
                     </div>
                 </div>
+                <p class="clearfix xs-separator"></p>
                 <div class="col-sm-4 col-xs-6">
                     <div align="center">
                         <img src="/images/features/people.png">
                         <h2>People</h2>
+                        <p>Search Malaysian living in UK easily based on the criterias provided.</p>
                     </div>
                 </div>
+                <p class="clearfix sm-separator"></p>
                 <div class="col-sm-4 col-xs-6">
-                    <div align="center">
+                    <div align="center" class="comingsoon">
+                        <div class="label label-warning">Coming Soon</div>
                         <img src="/images/features/messaging.png">
                         <h2>Messaging</h2>
+                        <p>Interact with people through Katsitu's secure Private Message.</p>
                     </div>
                 </div>
+                <p class="clearfix xs-separator"></p>
                 <div class="col-sm-4 col-xs-6">
-                    <div align="center">
+                    <div align="center"class="comingsoon">
+                        <div class="label label-warning">Coming Soon</div>
                         <img src="/images/features/event.png">
                         <h2>Event</h2>
+                        <p>Keep updated with Malaysian's event that happened around UK.</p>
                     </div>
                 </div>
                 <div class="col-sm-4 col-xs-6">
-                    <div align="center">
+                    <div align="center"class="comingsoon">
+                        <div class="label label-warning">Coming Soon</div>
                         <img src="/images/features/accomodation.png">
                         <h2>Accomodation</h2>
+                        <p>Discover and pick Malaysian's hosts easily or become one.</p>
                     </div>
                 </div>
             </div>
