264a3a878e8dc6580340679a7933f614211b049e
Faiz Shukri <faizshukri90@gmail.com>
2015-08-08 04:50:36 +0100

    Update verify email template, and change to plain

--- resources/views/email/verify.blade.php
+++ resources/views/email/verify.blade.php
@@ -1,14 +1,24 @@
-@extends('email.basic')
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <!-- If you delete this tag, the sky will fall on your head -->
+    <meta name="viewport" content="width=device-width" />
+</head>
+<body>
 
-@section('content')
-    <table>
-        <tr>
-            <td>
-                <h3>Hi, {{ $user->name }}</h3>
-                <p class="lead">Thanks for creating an account with the verification demo app.</p>
-                <p>Please follow the link below to verify your email address.</p>
-                <p class="callout"><a href="{{ url('register/verify/' . $user->confirmation_code) }}">{{ url('register/verify/' . $user->confirmation_code) }}</a></p>
-            </td>
-        </tr>
-    </table>
-@endsection
+    <p>Hi {{ $user->name }},</p>
+
+    <p class="lead">Thanks for creating an account with Katsitu.Com.</p>
+
+    <p>Please follow the link below to verify your email address.</p>
+
+    <p class="callout"><a href="{{ url('register/verify/' . $user->confirmation_code) }}">{{ url('register/verify/' . $user->confirmation_code) }}</a></p>
+
+    <p>Thanks,<br >
+        katsitu.com Administrator</p>
+
+    <p>Disclaimer:<br />
+        This is an auto-generated email. Please do not reply.</p>
+
+</body>
+</html>
