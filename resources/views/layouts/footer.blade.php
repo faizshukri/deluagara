25ecda1717fc362b02bb5263591e41cd393e1a20
Faiz Shukri <faizshukri90@gmail.com>
2015-08-11 00:19:41 +0100

    Fix footer view

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,15 +1,19 @@
 <p class="clearfix">&nbsp;</p>
 
-<hr style="margin: 0px;"/>
-<footer class="container">
-    <div class="pull-left" id="nav-bottom">
-        <ul class="nav navbar-nav">
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
+<footer>
+    <hr style="margin: 15px 0;"/>
+    <div class="container">
+        <div class="row">
+            <div class="pull-left" id="nav-bottom">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div id="developer">Katsitu.Com &copy; 2015 | All rights reserved</div>
+            <span id="siteseal" class="pull-right"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=b3d7rY5ImdZU7jt7tYhb4goMCWnWB5FiXEriT6oCtPHFryVm1bhbFat8OhNs"></script></span>
+        </div>
     </div>
-    <div id="developer">Katsitu.Com &copy; 2015 | All rights reserved</div>
-    <span id="siteseal" class="pull-right"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=b3d7rY5ImdZU7jt7tYhb4goMCWnWB5FiXEriT6oCtPHFryVm1bhbFat8OhNs"></script></span>
 </footer>
 
 <script src="{{ elixir('js/vendor.js') }}"></script>
