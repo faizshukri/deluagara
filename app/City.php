acbadb9dfa61f28b6ed188cd8df98a62f0d0463b
Faiz Shukri <faizshukri90@gmail.com>
2015-08-11 15:51:15 +0100

    Hide some fields in from City
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- app/City.php
+++ app/City.php
@@ -8,6 +8,8 @@ class City extends Model
 {
     protected $fillable = [ 'name' ];
 
+    protected $hidden = ['id', 'created_at', 'updated_at'];
+
     public function locations()
     {
         return $this->hasMany('Katsitu\Location');
