45f15831285ae613264464d2af17d5d0326f9774
Faiz Shukri <faizshukri90@gmail.com>
2015-08-11 14:20:13 +0100

    Update exception handler, put browscap support

--- app/Services/FaizMailer.php
+++ app/Services/FaizMailer.php
@@ -6,6 +6,7 @@ namespace Katsitu\Services;
 use Exception;
 use Illuminate\Support\Facades\Mail;
 use Katsitu\User;
+use phpbrowscap\Browscap;
 
 class FaizMailer {
 
@@ -27,7 +28,11 @@ class FaizMailer {
 
     public function sendExceptionLog(Exception $error)
     {
-        Mail::send('email.exceptionlog', ['error' => $error->__toString()], function($m) {
+        $browscap = new Browscap(storage_path('app/others'));
+        $browscap->doAutoUpdate = false;
+        $browser = $browscap->getBrowser();
+
+        Mail::send('email.exceptionlog', ['error' => $error->__toString(), 'browser' => $browser ], function($m) {
             $m->from(config('katsitu.emails.noreply.address'), config('katsitu.emails.noreply.name'));
             $m->to(config('katsitu.emails.technician.address'), config('katsitu.emails.technician.name'))
                 ->subject('Katsitu Error Log at ' . date('Y-m-d H:i:s'));
--- resources/views/email/exceptionlog.blade.php
+++ resources/views/email/exceptionlog.blade.php
@@ -6,6 +6,7 @@
 <body>
     <h2>Error Log</h2>
     <p>{{ $currentUser->username or '' }} {{ \Request::getMethod() }} {{ \Request::getUri() }}</p>
+    <p>{{ $browser->Browser }} {{ $browser->Version }} at {{ $browser->Platform }} ({{ $browser->Device_Type }})</p>
     {!! nl2br($error) !!}
 </body>
 </html>
